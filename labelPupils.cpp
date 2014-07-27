ab8d637418d79e296cad9b7768915d517732e271
Ben Jones <bentodjones@gmail.com>
2014-07-27 15:59:26 -0700

    Pupil labeling now working and emmitting to stdout.
    There is currently a problem with small images (crucially the images
    generated by samplePupils are affected) in that when the width is low
    enough that the window cannot be made small enough to fit, tracking does
    not match the window size.

--- labelPupils.cpp
+++ labelPupils.cpp
@@ -1,43 +1,110 @@
 #include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <math.h>
 
 using namespace cv;
 
-const int alpha_slider_max = 100;
-int alpha_slider;
-double alpha;
-double beta;
+const int defaultRadius = 10;
+const int minRadius = 5;
 
-Mat src1;
-Mat src2;
-Mat dst;
+Mat originalImage;
+int radius;
+bool drag;
+int dragX;
+int dragY;
+char * filename;
 
-void on_trackbar( int, void * ) {
-    alpha = (double) alpha_slider / alpha_slider_max;
-    beta =  ( 1.0 - alpha );
+int scaleFactor;
 
-    addWeighted( src1, alpha, src2, beta, 0.0, dst );
+bool imageMarked;
+int markX;
+int markY;
+int markRadius;
 
-    imshow( "Linear Blend", dst );
+void drawImage(int x, int y) {
+    Mat markedImage = originalImage.clone();
+    Point cursor(x , y);
+    circle( markedImage, cursor, radius, Scalar( 255, 0, 255 ) );
+
+    if (imageMarked) {
+        Point markCenter( markX, markY );
+        circle( markedImage, markCenter, markRadius, Scalar( 255, 255, 0) );
+    }
+
+    imshow("Mark Pupil", markedImage);
 }
 
-int main ( void ) {
-    src1 = imread( "./data/pupils/pupil_1406384442.png" );
-    src2 = imread( "./data/pupils/pupil_1406384448.png" );
+void save() {
+    if (imageMarked) {
+        printf("[%s] %d %d %d\n", filename, markX, markY, markRadius);
+    }
+    free(filename);
+    exit(0);
+}
 
-    if (!src1.data) { printf("Error loading src1 \n"); return -1; }
-    if (!src2.data) { printf("Error loading src2 \n"); return -1; }
+void call_back_function(int event, int x, int y, int , void* ) {
 
-    alpha_slider = 0;
+    if (drag) {
+        int newRadius = sqrt( (x-dragX)*(x-dragX) + (y-dragY)*(y-dragY) );
+        if (newRadius > minRadius) radius = newRadius;
+    } 
+    if ( event == EVENT_LBUTTONDOWN ) {
+        // Drag to set size with right click
+       dragX = x;
+       dragY = y;
+       drag = true;
+    } else if ( event == EVENT_LBUTTONUP ) {
+        // Set a marker with left click
+        markX = dragX;
+        markY = dragY;
+        drag = false;
+        markRadius = radius;
+        imageMarked = true;
+    } else if ( event == EVENT_MBUTTONUP ) {
+        // Delete markers with middle click
+        imageMarked = false;
+    } else if ( event == EVENT_RBUTTONUP ) {
+        // Save a marking with right click and exit
+        save();
+        exit(0);
 
-    namedWindow("Linear Blend", 1);
+    }
 
-    char TrackbarName[50];
-    printf( TrackbarName, "Alpha x %d", alpha_slider_max );
+    if (drag) {
+        drawImage(dragX, dragY);
+    } else {
+        drawImage(x, y);
+    }
+}
 
-    createTrackbar( TrackbarName, "Linear Blend", &alpha_slider, alpha_slider_max, on_trackbar );
 
-    on_trackbar( alpha_slider, 0 );
+int main ( int argc, char** argv ) {
+    if (argc != 2) {
+        printf("You must supply an image to load.");
+        return 1;
+    }
+    originalImage = imread( argv[1] );
+    radius = defaultRadius;
+    drag = false;
+    imageMarked = false;
+    markX = 0;
+    markY = 0;
+    dragX = 0;
+    dragY = 0;
+    markRadius = 0;
+
+    if (!originalImage.data) { printf("Error loading originalImage \n"); return -1; }
+
+    filename = (char*) malloc(strlen(argv[1]) + 1);
+    strcpy( filename, argv[1] );
+
+    namedWindow("Mark Pupil", WINDOW_NORMAL );
+    imshow("Mark Pupil", originalImage);
+    resizeWindow("MarkPupil", originalImage.cols * scaleFactor, originalImage.rows * scaleFactor);
+
+    setMouseCallback("Mark Pupil", call_back_function, NULL);
 
     waitKey(0);
+    free(filename);
     return 0;
 }
